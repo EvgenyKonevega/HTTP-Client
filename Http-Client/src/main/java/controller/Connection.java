07072d9173c840c3d323e955a4f2ba6b20fc833c
PolinaYA <PolinaYX9877@gmail.com>
2019-05-11 01:21:30 +0300

    check time in connection

--- Http-Client/src/main/java/controller/Connection.java
+++ Http-Client/src/main/java/controller/Connection.java
@@ -25,7 +25,13 @@ public class Connection {
 	private BufferedReader reader;
 
 	public String connection(String method, String params, HttpRequest httpRequest, String requesttext) throws BsuirException {
-        String result = httpRequest.request(requesttext, params, method);
+		long start = System.nanoTime();
+		// поиск смысла жизни ...
+		String result = httpRequest.request(requesttext, params, method);
+		long finish = System.nanoTime();
+		long timeConsumedMillis = finish - start;
+		System.out.println("Time for writing request____" + timeConsumedMillis);
+		
         System.out.println("____");
         System.out.println(result);
         System.out.println("____");
@@ -39,10 +45,27 @@ public class Connection {
             BufferedWriter bufferWriter = new BufferedWriter(fwriter);
             bufferWriter.write(result + "\n");
 
+            long start2 = System.nanoTime();
             socket = new Socket(httpRequest.getHost(), Integer.parseInt(httpRequest.getPort()));
+            long finish2 = System.nanoTime();
+    		long timeConsumedMillis2 = finish2 - start2;
+    		System.out.println("Time for socket = new Socket____" + timeConsumedMillis2);
+            
+            
+    		long start3 = System.nanoTime();
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            long finish3 = System.nanoTime();
+    		long timeConsumedMillis3 = finish3 - start3;
+    		System.out.println("Time for reader = new BufferedReader(new InputStreamReader(socket.getInputStream())__" + timeConsumedMillis3);
+            
+    		long start4 = System.nanoTime();
             writer = new PrintWriter(socket.getOutputStream(), true);
-
+            long finish4 = System.nanoTime();
+    		long timeConsumedMillis4 = finish4 - start4;
+    		System.out.println("Time for writer = new PrintWriter(socket.getOutputStream(), true)__" + timeConsumedMillis4);
+            
+            
+            
             if (socket == null) {
                 throw new BsuirException("no internet connection");
             }
@@ -50,15 +73,28 @@ public class Connection {
             writer.println(result);
             writer.flush();
             String str;
+            
+            long start5 = System.nanoTime();
             while ((str = reader.readLine()) != null) {
                 response.append(str + "\n");
             }
-
+            long finish5 = System.nanoTime();
+    		long timeConsumedMillis5 = finish5 - start5;
+    		System.out.println("Time for while__" + timeConsumedMillis5);
+            
+            
             writer.close();
             reader.close();
             socket.close();
 
+            
+            long start6 = System.nanoTime();
             bufferWriter.write("Response: \n " + response.toString() + "\n");
+            long finish6 = System.nanoTime();
+    		long timeConsumedMillis6 = finish6 - start6;
+    		System.out.println("Time for bufferWriter.write(\"Response: \\n \" + response.toString()__" + timeConsumedMillis6);
+            
+            
             bufferWriter.close();
 
             lOGGER.info("answer read successfuly");
